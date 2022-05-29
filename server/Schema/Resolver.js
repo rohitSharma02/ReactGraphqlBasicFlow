e0c6757bf482fc6435300dd83a91993d49474471
Rohit Sharma <rohit.sharma02@sjsu.edu>
2022-05-28 17:31:53 -0700

    Refactoring Errors

--- server/Schema/Resolver.js
+++ server/Schema/Resolver.js
@@ -1,11 +1,21 @@
 const userData = require("../MOCK_DATA.json");
+//const userData = require("../FakeData.js");
 
+// this is resolver map -> javascript object
+//using arrow function
+/*
+below arrow function equivalent to using function like:
+    function getAllUsers() {
+        return userData;
+    }
+*/
 const resolver = {
-    Query: {
-        getAllUser() {
-            return userData;
-        },
-    },
+  Query: {
+    getAllUsers: ()=> { 
+        return userData;
+    }
+  },
 };
+//console.log(userData);
 
 module.exports = { resolver };
